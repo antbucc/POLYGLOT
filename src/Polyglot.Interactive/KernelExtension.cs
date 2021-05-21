4958d65970ae03be5b1e0775b4bcf4c1529c3233
Diego Colombo <dicolomb@microsoft.com>
2021-05-21 12:52:47 +0100

    prototipo formatter

--- src/Polyglot.Interactive/KernelExtension.cs
+++ src/Polyglot.Interactive/KernelExtension.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.DotNet.Interactive;
 using Microsoft.DotNet.Interactive.Formatting;
 using Polyglot.Core;
 using Polyglot.CSharp;
+
+using static Microsoft.DotNet.Interactive.Formatting.PocketViewTags;
 
 namespace Polyglot.Interactive
 {
@@ -25,7 +28,24 @@ namespace Polyglot.Interactive
         {
             targetKernel.UseGameEngine(httpClientFactory);
             Engine.Instance.RegisterKernel(targetKernel);
+            
+
+            Formatter.Register<GameStateReport>(( report,  writer)=>
+            {
+                var color = report.Points > 0 ? "green" : "red";
+                var coins = Enumerable.Range(0, (int)report.GoldCoins)
+                    .Select(_ => img[src: "https://upload.wikimedia.org/wikipedia/commons/d/d6/Gold_coin_icon.png", style: "height:1.5em"]);
+                var html = div[style: "width:100px"](
+                    table(
+                        tr(td("Current Level"), td[style: $"color:{color}; width:100px"](report.CurrentLevel)),
+                        tr(td("Coins"), td[style: $"color:{color}"](coins))
+                    )
+                );
+                writer.Write(html);
+            }, HtmlFormatter.MimeType);
+
             Formatter.SetPreferredMimeTypeFor(typeof(GameStateReport), HtmlFormatter.MimeType);
+
             return Task.CompletedTask;
         }
     }
